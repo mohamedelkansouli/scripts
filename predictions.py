f2456e31438c06b839adb0135bc770b8488e635f
mohamedelkansouli <mohamed.elkansouli@gmail.com>
2020-07-04 03:29:42 +0200

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -1,8 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
+import requests 
+from bs4 import BeautifulSoup 
 import pandas as pd
 from datetime import datetime, timezone, timedelta
-import numpy as np
+import numpy as np 
 import time
 import matplotlib.pyplot as plt
 import random
@@ -23,16 +23,35 @@ prev=pd.DataFrame()
 result =pd.read_csv(r"/home/hduser/Documents/kafka/result/result.csv", header='infer')
 
 while True:
- #time.sleep(5)
+ #time.sleep(5)   
  for name in result['Name'].unique():
      # split into input (X) and output (y) variables
         #X, y = result.iloc[:,:-1],result.iloc[:,-1]
         result=result.drop(result[result['Last Price']=='-'].index)
         #result=result.drop(result[result['Name']=='MXN=X'].index)
-
+        
         X = pd.DataFrame(result.iloc[:,-1][result['Name']==name])
-
+        
         y = pd.DataFrame(result.iloc[:,1:3][result['Name']==name])
         #y_train = pd.DataFrame(encoder.fit_transform(y),columns=['Last Price'])
         #X_train = pd.DataFrame(encoder.fit_transform(X),columns=['date'])
+
+        
+        for a in y['Last Price']:
+            if ',' in a:
+                y['Last Price']= [a.replace('.','') for a in y['Last Price']]
+                y['Last Price']= [a.replace(',','.') for a in y['Last Price']]
                 
+        y['Last Price']=pd.to_numeric(y['Last Price'])
+        forecast=pd.DataFrame()
+       
+        forecast['Prev']=[mean(y['Last Price'][-3:])]
+        forecast['date']=datetime.now().strftime("%X")
+        forecast['Name']=name
+ 
+        prev=pd.concat([prev, forecast])      
+        #print(prev)
+        #plt.plot(prev[prev['Name']==name]['date'],prev[prev['Name']==name]['Last Price'])
+        #plt.show()        
+ prev.to_csv('/home/hduser/Documents/kafka/result/prev.csv',header=True)       
+    
