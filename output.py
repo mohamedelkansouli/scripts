14e6c14b8df0fb58f1e5aea8a9e082a1b61378bb
mohamedelkansouli <mohamed.elkansouli@gmail.com>
2020-08-22 01:48:39 +0200

    Update output.py

--- output.py
+++ output.py
@@ -5,19 +5,28 @@ while True:
 
     result =pd.read_csv(r"/home/hduser/Documents/kafka/result/result.csv", header='infer')
 
-    prev =pd.read_csv(r"/home/hduser/Documents/kafka/result/prev.csv", header='infer')
+    result.columns = ['index','Name','Last Price','Date']
+    prev =pd.read_csv(r"/home/hduser/Documents/kafka/result/prev.csv" ,names=['Prev','Date','Name'])
+    prev=prev.drop_duplicates()
 
-    output=result.merge(prev, how='inner', left_on=["date","Name"], right_on=["date","Name"])
-    output=output[['date','Name','Last Price','Prev']]
+    output=result.merge(prev, how='inner', left_on=["Date","Name"], right_on=["Date","Name"])
+    output=output[['Date','Name','Last Price','Prev']]
+    output['Last Price_org']=output['Last Price']
+    
 
+    
 
+    output['erreur absolue']=(abs(output['Last Price']-output['Prev'])/output['Last Price'])*100
+    
+    #print (output.dtypes)
+    
+    output=output.drop_duplicates()
 
-
-
-    output.to_csv('/home/hduser/Documents/kafka/result/output.csv',header=True)
-
+    output.to_csv('/home/hduser/Documents/kafka/result/output.csv',mode='a',header=False)
+    #del result 
+    #del prev 
     #exit()
     #output.to_csv('/home/hduser/Documents/kafka/result/output.csv',header=True)
 
   except ValueError:
-    print ("Erreur")
+    print (ValueError)  
