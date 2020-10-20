7a841f77e2f55cad6c55969504b6c096410f6887
mohamedelkansouli <mohamed.elkansouli@gmail.com>
2020-10-20 13:12:32 +0200

    update

--- createMACD.py
+++ createMACD.py
@@ -1,34 +1,38 @@
 def createMACD(result):
     
-    result['e26'] = pd.Series.ewm(result['Last Price'], span=26).mean()
-    result['e12'] = pd.Series.ewm(result['Last Price'], span=12).mean()
-    result['MACD'] = result['e12'] - result['e26']
+    result['e26'] = pd.Series.ewm(result['Last Price'][result['Coin']==name], span=26).mean()
+    result['e12'] = pd.Series.ewm(result['Last Price'][result['Coin']==name], span=12).mean()
+    result['MACD'] = result['e12'][result['Coin']==name] - result['e26'][result['Coin']==name]
     return result
 
-result = createMACD(result)
 
 def computeStrategy(result):
-    # initialisation de quelques variables
-    profit = 0
-    move = 'buy' # drapeau qui servira à signaler le prochain mouvement 'buy' > 'sell' > 'buy' etc...
+        # initialisation de quelques variables
+        profit = 0
+        move = 'buy' # drapeau qui servira à signaler le prochain mouvement 'buy' > 'sell' > 'buy' etc...
+        
+        # crée des colonnes vides dans laquelle nous placerons notre strategie et notre budget
+        result['position'] = None
+        result['budget'] = 0
+        
+        
+for name in result['Coin'].unique():
+    result[result['Coin']==name] = createMACD(result[result['Coin']==name])
     
-    # crée des colonnes vides dans laquelle nous placerons notre strategie et notre budget
-    result['position'] = None
-    result['budget'] = 0
-    
-for row in range (len(result)): 
-    # conditions pour un achat
-    if result['MACD'].iloc[row] < 0 and result['MACD'].iloc[row-1] > 0 and move == 'buy':
-        result['position'].iloc[row] = 'buy'
-        move = 'sell'
-        lastLast Price = result['Last Price'].iloc[row]
-        result['budget'].iloc[row] = result['budget'].iloc[row-1] - result['Last Price'].iloc[row]
-    # conditions pour une vente
-    elif result['MACD'].iloc[row] > 0 and result['MACD'].iloc[row-1] < 0 and move == 'sell' and result['Last Price'].iloc[row] > lastLast Price:
-        result['position'].iloc[row] = 'sell'
-        move = 'buy'
-        result['budget'].iloc[row] = result['budget'].iloc[row-1] + result['Last Price'].iloc[row]
-    # ni vente ni achat, nous tenons la position
-    else:
-        result['position'].iloc[row] = 'hold'
-        result['budget'].iloc[row] = result['budget'].iloc[row-1]
+        
+    for row in range (len(result[result['Coin']==name])): 
+        # conditions pour un achat
+        if result['MACD'][result['Coin']==name].iloc[row] < 0 and result['MACD'][result['Coin']==name].iloc[row-1] > 0 and move == 'buy':
+            result['position'][result['Coin']==name].iloc[row] = 'buy'
+            move = 'sell'
+            lastClose  = result['Last Price'][result['Coin']==name].iloc[row]
+            result['budget'][result['Coin']==name].iloc[row] = result['budget'][result['Coin']==name].iloc[row-1] - result['Last Price'].iloc[row]
+        # conditions pour une vente
+        elif result['MACD'].iloc[row] > 0 and result['MACD'].iloc[row-1] < 0 and move == 'sell' and result['Last Price'].iloc[row] > lastClose :
+            result['position'].iloc[row] = 'sell'
+            move = 'buy'
+            result['budget'].iloc[row] = result['budget'].iloc[row-1] + result['Last Price'].iloc[row]
+        # ni vente ni achat, nous tenons la position
+        else:
+            result['position'].iloc[row] = 'hold'
+            result['budget'].iloc[row] = result['budget'].iloc[row-1]
